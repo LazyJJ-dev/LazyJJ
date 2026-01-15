3f761b9498855344e571d73991eabd4bc9c6bc4c
Ernesto Jiménez <me@ernesto-jimenez.com>
2026-01-15 23:54:29 +0000

    cleanup and deploys

--- website/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_zeeb4 from "../../assets/logo-light.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/logo-light.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_zeeb4]]);
-		
--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lazyjj-dev.github.io',
+  base: '/lazyjj',
   integrations: [
     starlight({
       title: 'LazyJJ',
